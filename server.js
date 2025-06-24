de6a0a81191fd541530f76f50c963ae1098bb90b
brenda <bmoleta_2000@yahoo.com>
2025-06-24 19:28:32 +0200

    commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
+//const port = process.env.PORT || 8000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
-// server.listen(process.env.PORT || 3000, () => {
-//   console.log("JSON Server is running");
-// });
+server.listen(process.env.PORT || 3000, () => {
+  console.log("JSON Server is running");
+});
 
-server.listen(port);
+//server.listen(port);
